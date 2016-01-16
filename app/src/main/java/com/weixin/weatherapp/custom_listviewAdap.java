b26ade158ca25dce331bb6b0bfd5d9a86de68a44
weixinwu <wuweixin6@gmail.com>
2016-01-16 12:55:20 -0500

    /

---
+++ app/src/main/java/com/weixin/weatherapp/custom_listviewAdap.java
@@ -0,0 +1,34 @@
+package com.weixin.weatherapp;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Weixin on 2016-01-07.
+ */
+public class custom_listviewAdap extends ArrayAdapter<String> {
+    String[] sec_text;
+    public custom_listviewAdap(Context context, String[] str,String []str2) {
+        super(context,R.layout.listview_item_for_detail,str);
+        this.sec_text = str2;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater layoutInflater = LayoutInflater.from(getContext());
+        View v = layoutInflater.inflate(R.layout.listview_item_for_detail, parent, false);
+        TextView tv1=(TextView)v.findViewById(R.id.tv_left);
+        TextView tv2=(TextView)v.findViewById(R.id.tv_right);
+
+        tv1.setText(getItem(position));
+        tv2.setText(sec_text[position]);
+        return v;
+    }
+}
